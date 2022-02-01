cd175ad5fa9f0aca7a1e0b0453d4f9f0fc5f5cb9
melcam <yumelcam.93@gmail.com>
2022-02-01 00:41:49 -0500

    Peticiones completas

--- src/main/java/com/yanki/monedera/expose/YankiController.java
+++ src/main/java/com/yanki/monedera/expose/YankiController.java
@@ -2,15 +2,20 @@ package com.yanki.monedera.expose;
 
 import com.yanki.monedera.business.YankiService;
 import com.yanki.monedera.model.Yanki;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -20,10 +25,42 @@ public class YankiController {
   @Autowired
   private YankiService yankiService;
 
+  @GetMapping("/api/yankis/{id}")
+  public Mono<Yanki> findById(@PathVariable("id") String id) {
+    log.info("<--findById-->");
+    return yankiService.findById(id);
+  }
+
+  @GetMapping("/api/yankis")
+  public Flux<Yanki> findAll() {
+    log.info("<--findAll-->");
+    return yankiService.findAll();
+  }
+
   @PostMapping("/api/yankis")
   @ResponseStatus(HttpStatus.CREATED)
-  public Mono<Yanki> create(@RequestBody Yanki yanki){
+  public Mono<Yanki> create(@RequestBody Yanki yanki) {
     log.info("<--create-->");
     return yankiService.create(yanki);
+  }
+
+  @PutMapping("/api/yankis")
+  public Mono<Yanki> update(@RequestBody Yanki yanki) {
+    log.info("<--update-->");
+    return yankiService.update(yanki);
+  }
+
+  @PatchMapping("/api/yankis")
+  public Mono<ResponseEntity<Yanki>> change(@RequestBody Yanki yanki) {
+    log.info("<--change-->");
+    return yankiService.change(yanki).flatMap(atUpdate -> Mono.just(ResponseEntity.ok(atUpdate)))
+        .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+  }
+
+  @DeleteMapping("/api/yankis/{id}")
+  public Mono<ResponseEntity<Yanki>> delete(@PathVariable("id") String id) {
+    log.info("<--delete-->");
+    return yankiService.delete(id).flatMap(at -> Mono.just(ResponseEntity.ok(at)))
+        .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
   }
 }
